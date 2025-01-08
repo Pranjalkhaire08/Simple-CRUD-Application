d0eca9a014d4eebea7c96eadd3cb09a2bb789d70
Pranjalkhaire08 <pranjalkhaire8@gmail.com>
2025-01-08 11:56:01 +0530

    Add files via upload

---
+++ IStudentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentPortal.Web.Models;
+
+namespace StudentPortal.Web.Controllers
+{
+    public interface IStudentsController
+    {
+        IActionResult Add();
+        Task<IActionResult> Add(AddStudentViewModel viewModel);
+        Task<IActionResult> List();
+    }
+}
